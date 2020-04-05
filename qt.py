8133e0e746bec3d2b1a1566577046f5b41650e61
v3arms <kirilchenko.aa@phystech.edu>
2020-04-05 17:50:17 +0300

    ui added

--- qt.py
+++ qt.py
@@ -1,28 +1,51 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QProgressBar, QFileDialog, QWidget, QTextEdit
+from PyQt5 import uic
+from base.base_inference import VideoInference
+from models import UNet
+from models import DeepLabV3Plus
+from subprocess import call
+from os import path
+from 
+import torch
+import ffmpeg
 
-# Subclass QMainWindow to customise your application's main window
+
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
+        uic.loadUi('ui.ui', self)
+        self.show()
+        
+        self.openBt   = self.findChild(QPushButton, 'openBt')
+        self.runBt    = self.findChild(QPushButton, 'runBt')
+        self.textEdit = self.findChild(QTextEdit, 'textEdit')
+        
+        self.openBt.clicked.connect(self.openButtonClicked)
+        self.runBt.clicked.connect(self.runButtonClicked)
+        
+        
+    def openButtonClicked(self) :
+        self.v_in, _ = QFileDialog.getOpenFileName(self, "Select video")
+        if self.v_in:
+            self.v_out = 
+        
+    
+    def runButtonClicked(self) :
+        pass
+ 
+ 
+    def updatePB(self) :
+        pass
+    
+        
+    def updateTE(self) :
+        pass
 
-        self.setWindowTitle("My Awesome App")
 
-        label = QLabel("This is a PyQt5 window!")
-
-        # The `Qt` namespace has a lot of attributes to customise
-        # widgets. See: http://doc.qt.io/qt-5/qt.html
-        label.setAlignment(Qt.AlignCenter)
-
-        # Set the central widget of the Window. Widget will expand
-        # to take up all the space in the window by default.
-        self.setCentralWidget(label)
 
 
 app = QApplication(sys.argv)
-
 window = MainWindow()
 window.show()
-
 app.exec_()
