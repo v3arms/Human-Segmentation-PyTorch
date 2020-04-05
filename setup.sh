18c68369a5ca0913e2ad1c3ec138f7ec025504b2
v3arms <kirilchenko.aa@phystech.edu>
2020-04-05 11:58:13 +0300

    reqs corrected

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
+eval "$(conda shell.bash hook)"
+
 conda create -n humanseg python=3.6
 conda activate humanseg
-conda install pytorch==1.2.0 cudatoolkit==9.2
-conda install Pillow
-pip install ffmpeg-python
-pip install -r requirements.txt -U
+pip install -r requirements.txt
